a4f3563f1519891dd0561c7e80328f37027c2190
Shocademy | Frontend Developer <shocademy@gmail.com>
2025-02-26 07:54:15 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/github-mark-white.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon3 from '../assets/img/Linktree_logo_white.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -63,7 +63,7 @@ export const NavBar = () => {
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/scriptbal/" onMouseEnter={playClickSound}><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/shocademy" onMouseEnter={playClickSound}><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/shocademy" onMouseEnter={playClickSound}><img src={navIcon3} alt="" /></a>
+                <a href="https://linktr.ee/shocademy" onMouseEnter={playClickSound}><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <a href="#connect"><button onMouseEnter={playClickSound} className="vvd"><span>Get In Touch</span></button></a>
